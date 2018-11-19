22f9530fcf322e9ac0ff0f2f1ec7e32452d6c1fc
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-11-19 13:14:24 -0600

    stashing to checkout master

--- client/routes.js
+++ client/routes.js
@@ -1,67 +1,68 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
 import {
-	MyList,
-	MyLists,
-	Signup,
-	UserHome,
-	Preferences,
-	SingleRecipe,
-	LoginForm,
-} from './components';
-import { me } from './store';
+  MyList,
+  MyLists,
+  UserHome,
+  Preferences,
+  SingleRecipe,
+  LoginForm
+} from "./components";
+import { me } from "./store";
 
 //component
 class Routes extends Component {
-	componentDidMount() {
-		this.props.loadInitialData();
-	}
-	render() {
-		const { isLoggedIn } = this.props;
-		return (
-			<Switch>
-				{/* Routes placed here are available to all visitors */}
-				<Route exact path="/" component={LoginForm} />
-				<Route path="/signup" component={Signup} />
-				<Route path="/lists/:id" component={MyList} />
-				<Route
-					path="/recipes/singleview/:recipename"
-					component={SingleRecipe}
-				/>
-				<Route path="/lists/" component={MyLists} />
-				{isLoggedIn && (
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <div>
+        {!isLoggedIn && (
 					<Switch>
-						{/* Routes placed here are only available after logging in */}
-						<Route exact path="/home" component={UserHome} />
-						<Route path="/home/preferences" component={Preferences} />
+						<Route exact path="/" component={LoginForm} />
+						<Route path="/login" component={LoginForm} />
 					</Switch>
 				)}
-				{/* Displays our Login component as a fallback */}
-				<Route component={LoginForm} />
-			</Switch>
-		);
-	}
+        {isLoggedIn && (
+          <Switch>
+            {/* Routes placed here are only available after logging in */}
+            <Route exact path="/" component={UserHome} />
+            <Route exact path="/home" component={UserHome} />
+
+            <Route path="/home/preferences" component={Preferences} />
+
+            <Route path="/lists/" component={MyLists} />
+            <Route path="/lists/:id" component={MyList} />
+
+            <Route
+              path="/recipes/singleview/:recipename"
+              component={SingleRecipe}
+            />
+          </Switch>
+        )}
+      </div>
+    );
+  }
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
-	return {
-		// Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-		// Otherwise, state.user will be an empty object, and state.user.id will be falsey
-		isLoggedIn: !!state.user.id,
-	};
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+    isLoggedIn: !!state.user.id
+  };
 };
 
 const mapDispatch = dispatch => {
-	return {
-		loadInitialData() {
-			dispatch(me());
-		},
-	};
+  return {
+    loadInitialData() {
+      dispatch(me());
+    }
+  };
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
@@ -72,6 +73,6 @@ export default withRouter(connect(mapState, mapDispatch)(Routes));
  * PROP TYPES
  */
 Routes.propTypes = {
-	loadInitialData: PropTypes.func.isRequired,
-	isLoggedIn: PropTypes.bool.isRequired,
+  loadInitialData: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
 };
