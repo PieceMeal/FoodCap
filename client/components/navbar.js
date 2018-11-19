a38b853b32ff364032f7e5dc61d9c032dc8ba787
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-11-19 12:09:57 -0600

    formatted navbar and added it back in to the home view

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,33 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
-import { Container, Menu, Button, Image } from 'semantic-ui-react';
+import { Menu, Button, Image } from 'semantic-ui-react';
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-	<Container>
-		<Menu size="large">
-			<Link to="/home">
-				<Image src='/logo.png' height="100" />
-			</Link>
-			<Menu.Menu position="right">
-				{isLoggedIn ? (
-					<Button primary onClick={handleClick}>
-						Log out
-					</Button>
-				) : (
-					<Menu.Item>
-						<Link to="/login">
-							<Button primary>Log In</Button>
-						</Link>
-
-						<Link to="/signup">
-							<Button primary>Sign Up</Button>
-						</Link>
-					</Menu.Item>
-				)}
-			</Menu.Menu>
+const Navbar = ({ handleClick }) => (
+		<Menu>
+			<Menu.Item>
+				<Link to="/home">
+					<Image src='/logo.png' height="100" />
+				</Link>
+			</Menu.Item>
+			<Menu.Item position="right">
+				<Button onClick={handleClick} position="right" color="green" >
+					Log out
+				</Button>
+			</Menu.Item>
 		</Menu>
-	</Container>
 );
 
 const mapState = state => {
--- client/components/userHome.js
+++ client/components/userHome.js
@@ -1,57 +1,70 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ListPreview from './ListPreview'
-import {connect} from 'react-redux'
-import {Container,Header, Icon, Divider, Grid,Card, Image,Button } from 'semantic-ui-react'
-import {fetchRecipes} from '../store/user'
+import React from "react";
+import PropTypes from "prop-types";
+import ListPreview from "./ListPreview";
+import { connect } from "react-redux";
+import Navbar from './navbar'
+import {
+  Container,
+  Header,
+  Icon,
+  Divider,
+  Grid,
+  Card,
+  Image,
+  Button
+} from "semantic-ui-react";
+import { fetchRecipes } from "../store/user";
 /**
  * COMPONENT
  */
 class UserHome extends React.Component {
-componentDidMount(){
-  this.props.fetchRecipes(this.props.user.id)
-}
-  render(){
-  if (this.props.recipes) {
-  return (
-   <Container>
-       <div>
-    <Header as='h2' icon textAlign='center'>
-      <Icon name='users' circular />
-      <Header.Content>Hello {this.props.email}</Header.Content>
-    </Header>
-  </div>
-  <Divider />
-  <ListPreview />
-  <Divider />
-  <Grid columns={4}>
-    <Grid.Row>
-   {this.props.recipes.map((rec, id) => {
-     console.log('this is my rec', rec)
-     return (
-      <Grid.Column key={id}>
-      <Card>
-    <Image src={rec.image} />
-    <Card.Content>
-      <Card.Header>{rec.name}</Card.Header>
-      <Container textAlign='right'><Button icon='upload' /></Container>
-      <Card.Meta>Time: {rec.time}</Card.Meta>
-
-    </Card.Content>
-  </Card>
-      </Grid.Column>
-     )
-   })}
-    </Grid.Row>
-  </Grid>
-  </Container>
-  )}
-  else {
-    //loading page ??
-    return <div/>
+  componentDidMount() {
+    this.props.fetchRecipes(this.props.user.id);
   }
-}
-
+  render() {
+    if (this.props.recipes) {
+      return (
+        <div>
+          <Navbar />
+          <Container>
+            <div>
+              <Header as="h2" icon textAlign="center">
+                <Icon name="users" circular />
+                <Header.Content>Hello {this.props.email}</Header.Content>
+              </Header>
+            </div>
+            <Divider />
+            <ListPreview />
+            <Divider />
+            <Grid columns={4}>
+              <Grid.Row>
+                {this.props.recipes.map((rec, id) => {
+                  console.log("this is my rec", rec);
+                  return (
+                    <Grid.Column key={id}>
+                      <Card>
+                        <Image src={rec.image} />
+                        <Card.Content>
+                          <Card.Header>{rec.name}</Card.Header>
+                          <Container textAlign="right">
+                            <Button icon="upload" />
+                          </Container>
+                          <Card.Meta>Time: {rec.time}</Card.Meta>
+                        </Card.Content>
+                      </Card>
+                    </Grid.Column>
+                  );
+                })}
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </div>
+      );
+    } else {
+      //loading page ??
+      return <div />;
+    }
+  }
 }
 /**
  * CONTAINER
@@ -59,19 +72,19 @@ componentDidMount(){
 const mapState = state => {
   return {
     email: state.user.email,
-    user : state.user,
+    user: state.user,
     recipes: state.user.recipes
-  }
-}
+  };
+};
 const dispatchState = dispatch => ({
-  fetchRecipes : (id) => dispatch(fetchRecipes(id))
-})
+  fetchRecipes: id => dispatch(fetchRecipes(id))
+});
 
-export default connect(mapState, dispatchState)(UserHome)
+export default connect(mapState, dispatchState)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
   email: PropTypes.string
-}
+};
